fbe84956040dd84557bcea7810fb622cb69d56a9
delwerHossain <delwerhossain006@gmail.com>
2023-02-22 15:03:40 +0000

    add

--- m-32.5/p-2.js
+++ m-32.5/p-2.js
@@ -3,20 +3,6 @@
 //   a) level-2
 //   b) 72.8621
 
-//   [
-//   Note: resources for 2D array:
-//   ====================
-//   1) anisul islam 2D array
-//   2) https://www.freecodecamp.org/news/javascript-2d-arrays/
-//   checkpoints:
-//   ===========
-//   1) Just know what is 2D array
-//   2) how to access an element in 2D array
-//   Then you will be able to do this task.
-//   ]
-
-//   Programming Hero
-
 let data = [
   {
     pHeroCourses: {
@@ -38,14 +24,58 @@ let data = [
       "course-xyz": "level-2",
     },
     locationField: {
-      "en-US": {
-        lat: 19.28563,
-        lon: 72.8691,
-      },
+        "en-US": {
+            lat: 19.28563,
+            lon: 72.8691,
+        },
     },
-  },
+},
 ];
+
 const x = data[3].pHeroCourses[0]
-// const 
+const b = x.['course-xyz']
+// const
 // ['course-xyz'] = x;
 console.log(x);
+//   [
+//   Note: resources for 2D array:
+//   ====================
+//   1) anisul islam 2D array
+//   2) https://www.freecodecamp.org/news/javascript-2d-arrays/
+//   checkpoints:
+//   ===========
+//   1) Just know what is 2D array
+//   2) how to access an element in 2D array
+//   Then you will be able to do this task.
+//   ]
+
+//   Programming Hero
+
+// let data = [
+//   {
+//     pHeroCourses: {
+//       "course-x": "web development",
+//     },
+//   },
+//   {
+//     pHeroCourses: {
+//       "course-y": "phitron",
+//     },
+//   },
+//   {
+//     pHeroCourses: {
+//       "course-z": "acc",
+//     },
+//   },
+//   {
+//     pHeroCourses: {
+//       "course-xyz": "level-2",
+//     },
+//     locationField: {
+//       "en-US": {
+//         lat: 19.28563,
+//         lon: 72.8691,
+//       },
+//     },
+//   },
+// ];
