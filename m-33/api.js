3cbd009a3056a09660bb828d5f533421385e28a3
delwerHossain <delwerhossain006@gmail.com>
2023-02-24 09:20:05 +0000

    add

--- m-33/api.js
+++ m-33/api.js
@@ -10,7 +10,31 @@ document.getElementById("test").addEventListener("click", function(){
 
 function getPost(json) { 
   for (post in json) {
+     let id= json[post].id;
      let title= json[post].title;
+    let body = json[post].body;
+    
+    const parent =document.getElementById("post")
+    const div = document.createElement("div")
+    div.innerHTML = `
+    <div class="card w-96 bg-base-100 shadow-xl">
+    <div class="card-body">
+
+      <h2 class="card-title">${id}</h2>
+      <h2 class="card-title">${title}</h2>
+      <p>${body}</p>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary">comment</button>
+      </div>
+    </div>
+  </div>`
+    
+    parent.appendChild(div)
+
+    
+    
+
+    
 
   }
 };
